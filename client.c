347d435bc5fffe93d60212302784a016f8a3af30
cklw0601 <cklw0601@gmail.com>
2024-04-18 13:51:54 -0400

    new

--- client.c
+++ client.c
@@ -8,6 +8,9 @@
 #include "threadpool.h"
 #define Total_no_of_partial_product 5 //for make to work I have defined this, you should comupte this value from the dimension of the input matrices
 
+static int i = 0;
+static int j = 0;
+
 struct data
 {
     //information needed for one thread to process 
@@ -41,8 +44,39 @@ int main(void)
     //read the files with input  martices an create the data
     // create some work to do
     struct data *work;
-    int i, rows, columns, **mat1, **mat2, **prod;
-    //dynamically allocate the array depending on the number of partial products
+    int i, rows, columns, **mat1, **mat2, **prod, symbol;
+    FILE *aFile = NULL, *bFile = NULL;
+    char fileName[100];
+
+    // open file A
+    aFile = fopen("A.txt", "r");
+
+    // finds the value of rows and columns for Matrix A
+    if (aFile != NULL)
+    {
+        do
+        {
+            symbol = fgetc(aFile);
+
+            if (rows == 0 && (symbol == " " || feof(aFile)))
+                columns++;
+            if (symbol == '\n' || feof(aFile))
+                rows++;
+        }
+        while (symbol != EOF);
+
+        if (ferror(aFile))
+            printf("ERROR: Could not read the file correctly.\n");
+        else
+            printf("\nMatrix A\nNumber of rows: %d\nNumber of columns: %d\n", rows, columns);
+        fclose(aFile);
+    }
+    else
+    {
+        perror("ERROR: Could not open the file correctly");
+    }
+
+    //dynamically allocate the array depending on the number of rows and columns for Matrix A
     work->row1 = rows;
     work->col1 = columns;
     mat1 = malloc(rows*sizeof(int*));
@@ -52,7 +86,36 @@ int main(void)
     }
     work->matrix1 = mat1;
 
+    
+    // open file B
+    bFile = fopen("B.txt", "r");
 
+    // finds the value of rows and columns for Matrix A
+    if (bFile != NULL)
+    {
+        do
+        {
+            symbol = fgetc(bFile);
+
+            if (rows == 0 && (symbol == " " || feof(bFile)))
+                columns++;
+            if (symbol == '\n' || feof(bFile))
+                rows++;
+        }
+        while (symbol != EOF);
+
+        if (ferror(bFile))
+            printf("ERROR: Could not read the file correctly.\n");
+        else
+            printf("\nMatrix B\nNumber of rows: %d\nNumber of columns: %d\n", rows, columns);
+        fclose(bFile);
+    }
+    else
+    {
+        perror("ERROR: Could not open the file correctly");
+    }
+
+    //dynamically allocate the array depending on the number of rows and columns for Matrix B
     work->row2 = rows;
     work->col2 = columns;
     mat2 = malloc(rows*sizeof(int*));
@@ -62,9 +125,8 @@ int main(void)
     }
     work->matrix2 = mat2;
 
-
-    work->row3 = work->col2;
-    work->col3 = columns;
+    work->row3 = work->row1;
+    work->col3 = work->col2;
     prod = malloc(rows*sizeof(int*));
     for(i = 0; i<rows; i++)
     {
@@ -72,6 +134,8 @@ int main(void)
     }
     work->product = prod;
 
+
+    
 
     // initialize the thread pool
     pool_init();
